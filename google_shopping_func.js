bbe9ccf9c4e415d052f5794f936f6db1be8ce68d
Nick Lathe <nicklathe@icloud.com>
2014-11-20 08:42:11 -0800

    Added a function for the bonus questions

--- google_shopping_func.js
+++ google_shopping_func.js
@@ -6,7 +6,7 @@ var data = require("./products.json")
 // 1). getItemsArray(array)
 
 var getItems = function() {
-	return(data.items);
+	return data.items;
 }
 
 // console.log(getItems());
@@ -66,6 +66,28 @@ console.log(getItemsByBrand(getItems(), "Sony"));
 // 5.3)
 
 console.log(getAvailableProducts(getItemsByBrand(getItems(), "Sony")));
+
+
+
+// BONUS FUNCTION 
+
+
+// This function looks for any item that has a price greater than $1000, and returns it in a new array.
+
+var expensiveItems = function(array) {
+	var listPriceArray = [];
+	for(var i = 0; i < array.length; i+=1) {
+		if(array[i].product.inventories[0].price > 1000) {
+			listPriceArray.push(array[i]);
+		}
+	}
+	return listPriceArray;
+}
+
+
+
+console.log(expensiveItems(getItemsByBrand(data.items, "Canon")));
+
 
 
 
